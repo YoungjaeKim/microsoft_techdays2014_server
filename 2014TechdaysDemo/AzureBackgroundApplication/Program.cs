9c0653df803abe0c73e59b327042f0324367ae3d
YoungjaeKim <youngjae.kim@gmail.com>
2014-09-24 10:47:19 +0900

    log test

--- 2014TechdaysDemo/AzureBackgroundApplication/Program.cs
+++ 2014TechdaysDemo/AzureBackgroundApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -17,56 +18,58 @@ namespace AzureBackgroundApplication
 			host.RunAndBlock();
 		}
 
+		public static void ProcessSimpleQueueMessage([QueueTrigger("webjobsqueue")] string inputText,
+			[Blob("tagboa/techdays.txt")]TextWriter writer)
+		{
+			writer.WriteLine(inputText);
+		}
+
 		/// <summary>
 		/// YouTube 동영상 주소로부터 이미지를 취득. 링크가 2개일 경우 좌우병합.
 		/// </summary>
 		/// <param name="input"></param>
 		/// <param name="BlobFilename"></param>
 		/// <param name="writer"></param>
-		public static void ProcessQueueMessage([QueueTrigger("webjobsqueue")] YoutubeLink input, 
-			string BlobFilename,
-			[Blob("tagboa/{BlobFilename}", FileAccess.Write)] Stream writer)
-		{
-			if (input.Links.Length == 1)
-			{
-				using (HttpClient client = new HttpClient())
-				{
-					var url = getVideoCode(input.Links[0]);
-					using (HttpResponseMessage response = client.GetAsync(url).Result)
-					{
-						if (response.IsSuccessStatusCode)
-						{
-							using (HttpContent content = response.Content)
-							{
-								var image = content.ReadAsByteArrayAsync().Result;
-								writer.Write(image, 0, image.Length);
-							}
-						}
-						else
-							Console.Out.WriteLine("cannot get from " + url);
-					}
-				}
-			}
-			else if (input.Links.Length == 2)
-			{
-				string[] links = input.Links.Select(getVideoCode).ToArray();
-				var messageBytes = ImageToByte(mergeImages(links));
+		//public static void ProcessQueueMessage([QueueTrigger("webjobsqueue")] YoutubeLink input, 
+		//	string BlobFilename,
+		//	[Blob("tagboa/{BlobFilename}", FileAccess.Write)] Stream writer)
+		//{
+		//	if (input.Links.Length == 1)
+		//	{
+		//		using (HttpClient client = new HttpClient())
+		//		{
+		//			var url = getVideoCode(input.Links[0]);
+		//			using (HttpResponseMessage response = client.GetAsync(url).Result)
+		//			{
+		//				if (response.IsSuccessStatusCode)
+		//				{
+		//					using (HttpContent content = response.Content)
+		//					{
+		//						var image = content.ReadAsByteArrayAsync().Result;
+		//						writer.Write(image, 0, image.Length);
+		//					}
+		//				}
+		//				else
+		//					Trace.WriteLine("cannot get from " + url);
+		//			}
+		//		}
+		//	}
+		//	else if (input.Links.Length == 2)
+		//	{
+		//		string[] links = input.Links.Select(getVideoCode).ToArray();
+		//		var messageBytes = ImageToByte(mergeImages(links));
 
-				writer.Write(messageBytes, 0, messageBytes.Length);
-			}
-			else
-			{
-				byte[] errorOutput = Encoding.UTF8.GetBytes("error");
-				writer.WriteAsync(errorOutput, 0, errorOutput.Length).Wait();
-			}
-			Console.Out.WriteLine("완료 :)");
+		//		writer.Write(messageBytes, 0, messageBytes.Length);
+		//	}
+		//	else
+		//	{
+		//		byte[] errorOutput = Encoding.UTF8.GetBytes("error");
+		//		writer.WriteAsync(errorOutput, 0, errorOutput.Length).Wait();
+		//	}
+		//	Trace.WriteLine("완료 :)");
 
-		}
+		//}
 
-		public static void WriteLog([QueueTrigger("webjobsqueue")] YoutubeLink input)
-		{
-			Console.Out.WriteLine("애저찡 사랑해 :)");
-		}
 
 		/// <summary>
 		/// 이미지 좌우 병합
