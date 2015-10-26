5ea7b53a11f7887c10c2c35d15d833377384a41e
YoungjaeKim <youngjae.kim@gmail.com>
2015-10-27 01:06:41 +0900

    hide complex job

--- 2014TechdaysDemo/AzureBackgroundApplication/Program.cs
+++ 2014TechdaysDemo/AzureBackgroundApplication/Program.cs
@@ -18,12 +18,12 @@ namespace AzureBackgroundApplication
 			host.RunAndBlock();
 		}
 
-		//public static void ProcessSimpleQueueMessage([QueueTrigger("webjobsqueue")] string inputText,
-		//	[Blob("techdays/techdays.txt")]TextWriter writer)
-		//{
-		//	writer.WriteLine(inputText);
-		//}
-		
+		public static void ProcessSimpleQueueMessage([QueueTrigger("webjobsqueue")] string inputText,
+			[Blob("techdays/techdays.txt")]TextWriter writer)
+		{
+			writer.WriteLine(inputText);
+		}
+
 
 		/// <summary>
 		/// YouTube 동영상 주소로부터 이미지를 취득. 링크가 2개일 경우 좌우병합.
@@ -31,46 +31,45 @@ namespace AzureBackgroundApplication
 		/// <param name="input"></param>
 		/// <param name="BlobFilename"></param>
 		/// <param name="writer"></param>
-		public static void ProcessQueueMessage(
-			[QueueTrigger("webjobsqueue")] YoutubeLink input,
-			string BlobFilename,
-			[Blob("techdays/{BlobFilename}", FileAccess.Write)] Stream writer)
-		{
-			if (input.Links.Length == 1)
-			{
-				using (HttpClient client = new HttpClient())
-				{
-					var url = getVideoCode(input.Links[0]);
-					using (HttpResponseMessage response = client.GetAsync(url).Result)
-					{
-						if (response.IsSuccessStatusCode)
-						{
-							using (HttpContent content = response.Content)
-							{
-								var image = content.ReadAsByteArrayAsync().Result;
-								writer.Write(image, 0, image.Length);
-							}
-						}
-						else
-							Trace.WriteLine("cannot get from " + url);
-					}
-				}
-			}
-			else if (input.Links.Length == 2)
-			{
-				string[] links = input.Links.Select(getVideoCode).ToArray();
-				var messageBytes = ImageToByte(mergeImages(links));
+		//public static void ProcessQueueMessage(
+		//	[QueueTrigger("webjobsqueue")] YoutubeLink input,
+		//	string BlobFilename,
+		//	[Blob("techdays/{BlobFilename}", FileAccess.Write)] Stream writer)
+		//{
+		//	if (input.Links.Length == 1)
+		//	{
+		//		using (HttpClient client = new HttpClient())
+		//		{
+		//			var url = getVideoCode(input.Links[0]);
+		//			using (HttpResponseMessage response = client.GetAsync(url).Result)
+		//			{
+		//				if (response.IsSuccessStatusCode)
+		//				{
+		//					using (HttpContent content = response.Content)
+		//					{
+		//						var image = content.ReadAsByteArrayAsync().Result;
+		//						writer.Write(image, 0, image.Length);
+		//					}
+		//				}
+		//				else
+		//					Trace.WriteLine("cannot get from " + url);
+		//			}
+		//		}
+		//	}
+		//	else if (input.Links.Length == 2)
+		//	{
+		//		string[] links = input.Links.Select(getVideoCode).ToArray();
+		//		var messageBytes = ImageToByte(mergeImages(links));
 
-				writer.Write(messageBytes, 0, messageBytes.Length);
-			}
-			else
-			{
-				byte[] errorOutput = Encoding.UTF8.GetBytes("error");
-				writer.WriteAsync(errorOutput, 0, errorOutput.Length).Wait();
-			}
-			Trace.WriteLine("완료 :)");
-
-		}
+		//		writer.Write(messageBytes, 0, messageBytes.Length);
+		//	}
+		//	else
+		//	{
+		//		byte[] errorOutput = Encoding.UTF8.GetBytes("error");
+		//		writer.WriteAsync(errorOutput, 0, errorOutput.Length).Wait();
+		//	}
+		//	Trace.WriteLine("완료 :)");
+		//}
 
 
 		/// <summary>
